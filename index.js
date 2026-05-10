753eca70e2f2e6969e78e4c43feb2d33f1ab0af5
nkododominique26-prog <nkododominique26@gmail.com>
2026-05-10 21:36:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 app.use(express.static('public'));
 app.use(express.json());
 
-const VPS_URL = 'http://162.248.100.46:3022'; 
+// Route test pour voir si le serveur répond
+app.get('/test', (req, res) => {
+    res.send("Le serveur fonctionne enfin !");
+});
 
 app.get('/', async (req, res) => {
     const user = { nom: "Dominique", solde: 2500 };
     try {
-        const response = await axios.get(`${VPS_URL}/articles`, { timeout: 3000 });
+        const response = await axios.get('http://162.248.100.46:3022/articles', { timeout: 3000 });
         res.render('index', { user: user, produits: response.data });
     } catch (error) {
         const produitsParDefaut = [
-            { id: 1, nom: "Netflix Premium", prix: 2500, image: "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" },
-            { id: 2, nom: "1000 Abonnés TikTok", prix: 1500, image: "https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg" }
+            { id: 1, nom: "Netflix", prix: 2500, image: "" },
+            { id: 2, nom: "TikTok", prix: 1500, image: "" }
         ];
         res.render('index', { user: user, produits: produitsParDefaut });
     }
@@ -27,5 +29,5 @@ app.get('/', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Serveur prêt sur le port ${PORT}`);
+    console.log("Serveur actif sur le port " + PORT);
 });
