941fa9248f9fc503cf8efecbb11f80ddd5a8521a
nkododominique26-prog <nkododominique26@gmail.com>
2026-05-12 00:46:02 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    coins: { type: Number, default: 0 }, // C'est ici qu'on stocke le solde
-    isAdmin: { type: Boolean, default: false },
+    coins: { type: Number, default: 0 },
+    role: { type: String, default: 'client' }, // 'client' ou 'admin'
     createdAt: { type: Date, default: Date.now }
 });
 
