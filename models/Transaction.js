2c17e18a89d8ad1968f5eae41e4a535b106d4234
nkododominique26-prog <nkododominique26@gmail.com>
2026-05-12 00:25:57 +0100

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     username: { type: String, required: true },
-    amount: { type: Number, required: true }, // Ex: 500
-    price: { type: Number, required: true },  // Ex: 500 FCFA
-    status: { type: String, default: 'En attente' }, // 'Validé' ou 'Refusé'
-    proofImage: { type: String }, // Lien vers la capture
+    amount: { type: Number, required: true }, // Nombre de Coins demandés
+    status: { type: String, default: 'En attente' }, // 'En attente', 'Validé', 'Refusé'
+    proofImage: { type: String }, // Nom du fichier image enregistré
     createdAt: { type: Date, default: Date.now }
 });
 
