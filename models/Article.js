ce99be4c5fc00dd238b430e89a4ff9bd3569245d
nkododominique26-prog <nkododominique26@gmail.com>
2026-05-12 12:55:36 +0100

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 const articleSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    content: { type: String, required: true },
-    price: { type: Number },
-    status: { type: String, default: 'En attente' }, // "En attente" ou "Approuvé"
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, enum: ['TikTok', 'Instagram', 'Facebook', 'IPTV', 'Autre'], default: 'Autre' },
     createdAt: { type: Date, default: Date.now }
 });
 
