c324850282e50526fa37de671138332fef373963
nkododominique26-prog <nkododominique26@gmail.com>
2026-05-12 15:11:14 +0100

    Update Deposit.js

--- models/Deposit.js
+++ models/Deposit.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const depositSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    amount: Number,
-    proofImage: String, // Lien vers l'image de la preuve
+    amount: { type: Number, required: true },
+    proofImage: { type: String, required: true },
     status: { type: String, enum: ['en_attente', 'valide', 'rejete'], default: 'en_attente' },
     createdAt: { type: Date, default: Date.now }
 });
