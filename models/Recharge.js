235791822f4e3ec73d9c7bae32e5430353d5cfb9
nkododominique26-prog <nkododominique26@gmail.com>
2026-05-12 04:11:06 +0100

    Update Recharge.js

--- models/Recharge.js
+++ models/Recharge.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const rechargeSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     amount: { type: Number, required: true },
-    method: { type: String, required: true }, // "Orange Money" ou "MTN MoMo"
-    status: { type: String, default: 'En attente' }, // "En attente", "Approuvé", "Rejeté"
+    method: { type: String, required: true },
+    proofImage: { type: String, required: true }, // URL Cloudinary
+    status: { type: String, default: 'En attente' }, // En attente, Approuvé
     createdAt: { type: Date, default: Date.now }
 });
 
